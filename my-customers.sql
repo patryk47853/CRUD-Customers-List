cb8861f9c2f608e79ad16051ac001fc2b74d0b81
patryk47853 <109095669+patryk47853@users.noreply.github.com>
2023-03-02 16:50:33 +0100

    Delete my-customers.sql

--- my-customers.sql
+++
@@ -1,5 +0,0 @@
-CREATE USER 'root'@'localhost' IDENTIFIED BY 'root';
-
-GRANT ALL PRIVILEGES ON * . * TO 'root'@'localhost';
-
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';
