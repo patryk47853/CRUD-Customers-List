7ba7e995e0f98d72469d881018cb1e9425c707dd
Patryk Maliszewski <p.maliszewski00@gmail.com>
2023-03-06 18:53:04 +0100

    Add CustomerService Implementation => add getCustomers() and searchCustomers() methods

---
+++ src/main/java/pl/patrykjava/service/CustomerService.java
@@ -0,0 +1,13 @@
+package pl.patrykjava.service;
+
+import pl.patrykjava.entity.Customer;
+
+import java.util.List;
+
+public interface CustomerService {
+
+    List<Customer> getCustomers();
+
+    List<Customer> searchCustomers(String customerName);
+
+}
---
+++ src/main/java/pl/patrykjava/service/CustomerServiceImpl.java
@@ -0,0 +1,28 @@
+package pl.patrykjava.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import pl.patrykjava.dao.CustomerDAO;
+import pl.patrykjava.entity.Customer;
+
+import java.util.List;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+    @Autowired
+    private CustomerDAO customerDAO;
+
+    @Override
+    @Transactional
+    public List<Customer> getCustomers() {
+        return customerDAO.getCustomers();
+    }
+
+    @Override
+    @Transactional
+    public List<Customer> searchCustomers(String customerName) {
+        return customerDAO.searchCustomers(customerName);
+    }
+}
