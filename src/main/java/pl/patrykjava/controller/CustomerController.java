c5ad2d66c68c67acf14e061ec18fd702f7396d4f
Patryk Maliszewski <p.maliszewski00@gmail.com>
2023-03-07 19:00:16 +0100

    Refactor "/saveCustomer" mapping to POST

--- src/main/java/pl/patrykjava/controller/CustomerController.java
+++ src/main/java/pl/patrykjava/controller/CustomerController.java
@@ -3,10 +3,7 @@ package pl.patrykjava.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import pl.patrykjava.dao.CustomerDAO;
 import pl.patrykjava.entity.Customer;
 import pl.patrykjava.service.CustomerService;
@@ -50,7 +47,7 @@ public class CustomerController {
         return "customer-form";
     }
 
-    @GetMapping("/saveCustomer")
+    @PostMapping("/saveCustomer")
     public String saveCustomer(@ModelAttribute("customer") Customer theCustomer) {
 
         customerService.saveCustomer(theCustomer);
