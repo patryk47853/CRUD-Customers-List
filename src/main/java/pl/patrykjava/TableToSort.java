9408db153280333173f7b4a6dd1422bd525e6956
Patryk Maliszewski <p.maliszewski00@gmail.com>
2023-03-10 17:24:29 +0100

    Provide final values for sorting options

---
+++ src/main/java/pl/patrykjava/TableToSort.java
@@ -0,0 +1,8 @@
+package pl.patrykjava;
+
+public class TableToSort {
+
+    public static final int FIRST_NAME = 1;
+    public static final int LAST_NAME = 2;
+    public static final int EMAIL = 3;
+}
